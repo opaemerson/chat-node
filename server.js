88ee07fac8e387969c264fe12320c509b60a615d
Emerson <quintiliano.emerson@hcosta.com.br>
2025-01-15 11:58:56 -0300

    att

--- server.js
+++ server.js
@@ -2,14 +2,13 @@ const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 8080 });
 
 wss.on('connection', (ws) => {
+  wss.clients.forEach((client) => {
+    if (client !== ws && client.readyState === WebSocket.OPEN) {
+      client.send('Alguem entrou no chat');
+    }
+  });
+
   ws.on('message', (message) => {
-
-    wss.clients.forEach((client) => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send('Alguém entrou no chat');
-      }
-    });
-
     wss.clients.forEach((client) => {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(message);
